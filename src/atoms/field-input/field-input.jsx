4171a2dae667e337d008a2217976de6210f69c55
Rafael Freitas <rafaelsfreitas283@gmail.com>
2023-10-17 20:51:04 -0300

    feat: funçao de trocar o icone ao clicar para visualizar a senha

--- src/atoms/field-input/field-input.jsx
+++ src/atoms/field-input/field-input.jsx
@@ -1,38 +1,45 @@
 import React, {useState} from "react";
 import olho from "../../assets/olho.png"
 import "./field-input.css"
-
-
+import { AiFillEye } from "react-icons/ai";
+import { AiFillEyeInvisible } from "react-icons/ai";
 
 
 function FieldInput({type, ...props}) {
     
-  const [showPassword, setShowPassword] = useState(false)
+    const [showPassword, setShowPassword] = useState(false)
     
     const passwordVisibility = () => {
       if (type === "password") {
         console.log("Clicou no ícone do olho");
         setShowPassword(!showPassword);
+        
       }
     };
    
-
     return(
         <div className="field">
+            
             <input className="input" {...props}
             type={showPassword && type ==="password" ? "text" : type}
             {...props}
             />
-            {type === "password" && (
-              <img 
-              src={olho}
-              alt="foto icone olho"
-              className="icon_eye"
-              onMouseDown={passwordVisibility}
-              />
-            )}
+
+            {type === "password" ? (
+              showPassword ? (
+                <AiFillEyeInvisible
+                  className="icon_eye"
+                  onClick={passwordVisibility}
+                />
+                ) : (
+                <AiFillEye
+                  className="icon_eye"
+                  onClick={passwordVisibility}
+                />
+                )
+            ) : null}
         </div>
-    );
+  );
 }
 
 export default FieldInput
