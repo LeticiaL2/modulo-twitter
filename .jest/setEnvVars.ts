f50660057d108166948f1220c6300fcd918ce75d
leticia <leticia.sampaio@digitalbusiness.com.br>
2023-10-26 11:57:07 -0300

    commit inicial

---
+++ .jest/setEnvVars.ts
@@ -0,0 +1,9 @@
+process.env.SERVER_PORT='3000'
+process.env.SENTRY_DNS=''
+process.env.SENTRY_ENVIRONMENT=''
+process.env.CPQD_URL="http://localhost:3333"
+process.env.CPQD_INSTITUTIONID='18'
+process.env.CPQD_INSTITUTIONACRONYM="gazincred"
+process.env.CPQD_USERNAME="integrationgazincred"
+process.env.CPQD_PASSWORD="Q1BxRCtzYWYuMjAxNA=="
+process.env.JWT_SECRET='DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.'
