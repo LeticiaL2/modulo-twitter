36f9f6941d783b2b7d9ffa27e750e8ab241dfd38
ThiagoSlvr <thiagosilvera09@gmail.com>
2023-12-08 13:30:05 -0300

    secure route v0

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
+import { TweetsModule } from './tweets/tweets.module';
 
 
 @Module({
   imports: [
     UsersModule, 
     DbModule, 
-    AuthModule
+    AuthModule, TweetsModule
   ],
   controllers: [],
   providers: [],
