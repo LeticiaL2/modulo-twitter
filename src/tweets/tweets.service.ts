d8d9edb52bf4062bdf76a047e6e6b94cb903eafb
ThiagoSlvr <thiagosilvera09@gmail.com>
2024-01-03 18:18:13 -0300

    retweets getTweets update

--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -1,57 +1,111 @@
 import { BadRequestException, ConsoleLogger, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
-import { CreateTweetDto } from './dto/create-tweets.dto';
+import { CreateTweetDto } from './dto/create-tweet.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Tweets } from './entity/tweets.entity';
-import { Users } from 'src/users/entity/users.entity';
+import { Tweet } from './entity/tweet.entity';
+import { User } from 'src/users/entity/user.entity';
 import { response } from 'express';
-import { Likes } from './entity/likes.entity';
-import { Comments } from './entity/comments.entity';
-import { Retweets } from './entity/retweets.entity';
+import { Like } from './entity/like.entity';
+import { Comment } from './entity/comment.entity';
+import { Retweet } from './entity/retweet.entity';
+import { identity } from 'rxjs';
+import { exit } from 'process';
+import { getSystemErrorMap } from 'util';
 
 
 @Injectable()
 export class TweetsService {
     constructor(
-        @InjectRepository(Tweets)
-        private tweetsRepository: Repository<Tweets>,
-        @InjectRepository(Users)
-        private usersRepository: Repository<Users>,
-        @InjectRepository(Likes)
-        private likesRepository: Repository<Likes>,
-        @InjectRepository(Comments)
-        private commentsRepository: Repository<Comments>,
-        @InjectRepository(Retweets)
-        private retweetsRepository: Repository<Retweets>,
+        @InjectRepository(Tweet)
+        private tweetsRepository: Repository<Tweet>,
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+        @InjectRepository(Like)
+        private likesRepository: Repository<Like>,
+        @InjectRepository(Comment)
+        private commentsRepository: Repository<Comment>,
+        @InjectRepository(Retweet)
+        private retweetsRepository: Repository<Retweet>,
     ) {}
 
     async getTweets(userId: number) {
 
-        const tweets =  await this.tweetsRepository.createQueryBuilder("tweet")
-            .leftJoinAndSelect("tweet.usuario", "usuario")
-            .leftJoin(Comments, "comentario", "comentario.tweetId = tweet.id")
-            .where("comentario.tweetId IS NULL")
-            .andWhere("tweet.excluido = false")
-            .getMany();
+        const tweets = await this.tweetsRepository
+            // dados tweet original
+            .createQueryBuilder("tweet")
+            .leftJoinAndSelect('tweet.usuario', 'usuario')
+            .leftJoinAndSelect('tweet.likes', 'like')
+            .leftJoinAndSelect('like.usuario', 'likeUsuario')
+            .leftJoinAndSelect('tweet.comentarios', 'comentario')
+            // dados tweetPai do retweet
+            .leftJoinAndSelect("tweet.rtwTweet", "rwtTweet")
+            .leftJoinAndSelect("rwtTweet.tweetPai", "tweetPaiDoRetweet")
+            .leftJoinAndSelect("tweetPaiDoRetweet.likes", "likesTweetPaiDoRetweet")
+            .leftJoinAndSelect("tweetPaiDoRetweet.comentarios", "commentsDoTweetPaiDoRetweet")
+            .leftJoinAndSelect("tweetPaiDoRetweet.usuario", "usuarioTweetPaiDoRetweet")
+            .leftJoinAndSelect("tweetPaiDoRetweet.retweets", "retweetsDoTweetPaiDoRetweet")
+            // isso eh pro retuitado
+            .leftJoinAndSelect("retweetsDoTweetPaiDoRetweet.retweet", "retweetDoTweetPaiDoRetweet")
+            .leftJoinAndSelect("retweetDoTweetPaiDoRetweet.usuario", "usuarioDoRetweetDoTweetPaiDoRetweet")
+            // dados dos retweets do tweetPai
+            .leftJoinAndSelect("tweet.retweets", "retweet")
+            .leftJoinAndSelect("retweet.retweet", "tweetPai")
+            //.leftJoinAndSelect("tweetPai.retweets", "retweetsDoTweetPai")
+            .leftJoinAndSelect("tweetPai.likes", "likesDoTweetPai")
+            .leftJoinAndSelect("tweetPai.comentarios", "commentsDoTweetPai")
+            .leftJoinAndSelect("tweetPai.usuario", "usuarioDoTweetPai")
+            // filtros
+            .orderBy('tweet.data_criacao', 'DESC')
+            .where("tweet.excluido = :excluido", { excluido: false })
+            .andWhere("tweet.id NOT IN (SELECT tweetId FROM Comentarios)")
+            .orderBy('tweet.data_criacao', 'DESC')
+            .getMany()
+        ;
 
+
+        // console.log('Retweet:', tweets[0])
+        // console.log('TweetPai do Retweet: ', tweets[0].rtwTweet[0]);
+        // console.log('--------------------------------------------------------------------------------')
+        // console.log('TweetPai: ', tweets[1])
+        // console.log('Retweets do TweetPai: ', tweets[1].retweets[0]);
+        // process.exit();
+    
 
         const formattedTweets = await Promise.all(tweets.map( async (tweet) => {
-            const likesCount = await this.getLikesCount(tweet.id);
-            const liked = await this.userLikedTweet(userId, tweet.id);
-            const commentsCount = await this.getCommentsCount(tweet.id);
 
-            return {
-                id: tweet.id,
-                texto: tweet.texto,
-                usuarioId: tweet.usuario.id,
-                usuario: tweet.usuario.usuario,
-                nome: tweet.usuario.nome,
-                likes: likesCount, 
-                liked: liked,
-                comentarios: commentsCount,
-                retweets: null, //TODO
-                data: tweet.data_criacao,
-            };
+            try {
+                return {
+                    id: tweet.id,
+                    texto: tweet.texto,
+                    usuarioId: tweet.usuario.id,
+                    usuario: tweet.usuario.usuario,
+                    nome: tweet.usuario.nome,
+                    likes: tweet.likes.length,
+                    liked: tweet.likes.some(like => like.usuario.id === userId),
+                    comentarios: tweet.comentarios.length,
+                    retweets: tweet.retweets.length,
+                    retuitado: tweet.retweets.some(retweet => retweet.retweet.usuario.id === userId),
+                    idRetweet: null, //id do retweet ja esta no id do tweetPai
+                    tweetPai: tweet.rtwTweet[0]?.tweetPai ? {
+                        id: tweet.rtwTweet[0].tweetPai.id,
+                        texto: tweet.rtwTweet[0].tweetPai.texto,
+                        usuarioId: tweet.rtwTweet[0].tweetPai.usuario.id,
+                        usuario: tweet.rtwTweet[0].tweetPai.usuario.usuario,
+                        nome: tweet.rtwTweet[0].tweetPai.usuario.nome,
+                        likes: tweet.rtwTweet[0].tweetPai.likes.length,
+                        liked: tweet.rtwTweet[0].tweetPai.likes.some(like => like.usuario.id === userId), // falta isso na arquitetura?
+                        comentarios: tweet.rtwTweet[0].tweetPai.comentarios.length,
+                        retweets: tweet.rtwTweet[0].tweetPai.retweets.length,
+                        retuitado: tweet.rtwTweet[0].tweetPai.retweets.some(retweet => retweet.retweet.usuario.id === userId),
+                        data: tweet.rtwTweet[0].tweetPai.data_criacao
+                    } : null,
+                    data: tweet.data_criacao
+                };
+            } catch (error) {
+                console.error('Error while setting up return object:', error);
+                console.log('tweet:', tweet);
+                process.exit();
+            }
         }));
     
 
@@ -221,7 +275,7 @@ export class TweetsService {
         const newTweet = this.tweetsRepository.create({ ...createTweetDto, usuario: user });
         await this.tweetsRepository.save(newTweet);
     
-        const newComment = this.commentsRepository.create({ tweetPai, tweet: newTweet });
+        const newComment = this.commentsRepository.create({ tweetPai, comentario: newTweet });
         await this.commentsRepository.save(newComment);
     
         const response = {
@@ -263,30 +317,32 @@ export class TweetsService {
             where: { 
                 tweetPai: { id: tweetId },
             },
-            relations: ['tweetPai', 'tweet', 'tweet.usuario'],
+            relations: ['tweetPai', 'cmtTweet', 'cmtTweet.usuario'],
         });
     
         const formattedComments = await Promise.all(comments.map(async comment => {
-            const likesCount = await this.getLikesCount(comment.tweet.id);
-            const commentsCount = await this.getCommentsCount(comment.tweet.id);
+            const likesCount = await this.getLikesCount(comment.comentario.id);
+            const commentsCount = await this.getCommentsCount(comment.comentario.id);
+            const retweetsCount = await this.getRetweetsCount(comment.comentario.id);
             
         
             return {
-                id: comment.tweet.id,
-                texto: comment.tweet.texto,
-                usuarioId: comment.tweet.usuario.id,
-                usuario: comment.tweet.usuario.usuario,
-                nome: comment.tweet.usuario.nome,
+                id: comment.comentario.id,
+                texto: comment.comentario.texto,
+                usuarioId: comment.comentario.usuario.id,
+                usuario: comment.comentario.usuario.usuario,
+                nome: comment.comentario.usuario.nome,
                 likes: likesCount,
                 comentarios: commentsCount,
-                retweets: 0, //TODO
-                data: comment.tweet.data_criacao
+                retweets: retweetsCount,
+                data: comment.comentario.data_criacao
             };
         }));
 
 
         const likesCount = await this.getLikesCount(tweet.id);
         const commentsCount = await this.getCommentsCount(tweet.id);
+        const retweetsCount = await this.getRetweetsCount(tweet.id);
     
         const response = {
             status: true,
@@ -303,7 +359,7 @@ export class TweetsService {
                 likes: likesCount,
                 comentarios: commentsCount,
                 comentariosLista: formattedComments,
-                retweets: 0, //TODO
+                retweets: retweetsCount,
                 data: tweet.data_criacao
             }
         };
@@ -315,6 +371,7 @@ export class TweetsService {
 
 
     async postRetweet(tweetId: number, createTweetDto: CreateTweetDto, userId: number) {
+        
         const tweetPai = await this.tweetsRepository.findOne({ where: { id: tweetId } });
     
         if (!tweetPai) {
@@ -330,7 +387,7 @@ export class TweetsService {
         const newTweet = this.tweetsRepository.create({ ...createTweetDto, usuario: user });
         await this.tweetsRepository.save(newTweet);
     
-        const newRetweet = this.retweetsRepository.create({ tweetPai, tweet: newTweet });
+        const newRetweet = this.retweetsRepository.create({ tweetPai, retweet: newTweet });
         await this.retweetsRepository.save(newRetweet);
     
         const response = {
@@ -355,10 +412,8 @@ export class TweetsService {
         return response;
     }
     
-      
 
-
-
+    // Usada na getTweetDetails, passar para la
     async getLikesCount(tweetId: number): Promise<number> {
         return this.likesRepository.count({ 
             where: { 
@@ -367,6 +422,7 @@ export class TweetsService {
         });
     }
 
+    // Usada na getTweetDetails, passar para la
     async getCommentsCount(tweetId: number): Promise<number> {
         return this.commentsRepository.count({ 
             where: { 
@@ -375,6 +431,17 @@ export class TweetsService {
         });
     }
 
+
+    // Usada na getTweetDetails, passar para la
+    async getRetweetsCount(tweetId: number): Promise<number> {
+        return this.retweetsRepository.count({ 
+            where: { 
+                tweetPai: { id: tweetId } 
+            }
+        });
+    }
+
+    // Passar para dentro da função de like
     async userLikedTweet(userId: number, tweetId: number): Promise<boolean> {
         const like = await this.likesRepository.findOne({ 
             where: { 
@@ -384,5 +451,15 @@ export class TweetsService {
         });
         
         return like ? true : false;
+    }
+
+    // Nao utilizada
+    async userRetweetedTweet(userId: number, tweetId: number) {
+        const retweet = await this.retweetsRepository.createQueryBuilder("retweet")
+            .where("retweet.tweetId = :tweetId", { tweetId })
+            .andWhere("retweet.usuarioId = :userId", { userId })
+            .getOne();
+    
+        return !!retweet;
     }
 }
