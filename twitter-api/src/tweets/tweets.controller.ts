2e949bbf5afe48ba141415aa7ef3bdca5f929a95
AryaneLadeira <aryanemottals@gmail.com>
2024-01-11 12:34:50 -0300

    Adicionando o codigo http nos responses

--- twitter-api/src/tweets/tweets.controller.ts
+++ twitter-api/src/tweets/tweets.controller.ts
@@ -3,20 +3,22 @@ import {
 	Controller,
 	Delete,
 	Get,
+	HttpStatus,
 	NotFoundException,
 	Param,
 	Post,
 	Query,
+	Res,
 	UnauthorizedException,
 	UseGuards,
 	ValidationPipe,
 } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { CriarTweetDto } from './dto/criar-tweet.dto';
-import { RetornoTweetDto } from './dto/retorno-tweet.dto';
 import { GetIdUsuario } from 'src/usuarios/decorator/get-id-usuario.decorator';
 import { AuthGuard } from '@nestjs/passport';
 import { EncontrarTweetsParametrosDto } from './dto/encontrar-tweets-parametros.dto';
+import { Response } from 'express';
 @Controller('tweets')
 export class TweetsController {
 	constructor(private tweetsService: TweetsService) {}
@@ -26,26 +28,28 @@ export class TweetsController {
 	async criarTweet(
 		@Body(ValidationPipe) criarTweetDto: CriarTweetDto,
 		@GetIdUsuario() idUsuario: string,
-	): Promise<RetornoTweetDto> {
+		@Res() res: Response,
+	): Promise<Response> {
 		try {
 			const tweet = await this.tweetsService.criarTweet(criarTweetDto, idUsuario);
-			return {
+
+			return res.status(HttpStatus.CREATED).json({
 				conteudo: tweet,
 				mensagem: {
 					codigo: 201,
 					texto: 'Tweet criado com sucesso.',
 				},
 				status: true,
-			};
+			});
 		} catch (error) {
-			return {
+			return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
 				conteudo: null,
 				mensagem: {
 					codigo: 500,
 					texto: 'Erro interno do servidor',
 				},
 				status: false,
-			};
+			});
 		}
 	}
 
@@ -54,82 +58,86 @@ export class TweetsController {
 	async deletarUsuario(
 		@GetIdUsuario() idUsuario: string,
 		@Param('id') id: string,
+		@Res() res: Response,
 	) {
 		try {
 			await this.tweetsService.deletarTweet(id, idUsuario);
-			return {
+			return res.status(HttpStatus.OK).json({
 				conteudo: null,
 				mensagem: {
 					codigo: 200,
 					texto: 'Tweet removido com sucesso',
 				},
 				status: true,
-			};
+			});
 		} catch (error) {
-			if (error instanceof NotFoundException)
-				return {
+			if (error instanceof NotFoundException) {
+				return res.status(HttpStatus.NOT_FOUND).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 404,
 						texto: 'Tweet não encontrado',
 					},
 					status: false,
-				};
-			else if (error instanceof UnauthorizedException)
-				return {
+				});
+			} else if (error instanceof UnauthorizedException) {
+				return res.status(HttpStatus.UNAUTHORIZED).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 401,
 						texto: 'Você não tem autorização para excluir esse tweet',
 					},
 					status: false,
-				};
-			else {
-				return {
+				});
+			} else {
+				return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 500,
 						texto: 'Erro interno do servidor',
 					},
 					status: false,
-				};
+				});
 			}
 		}
 	}
 
 	@Get()
 	@UseGuards(AuthGuard())
-	async encontrarTweets(@Query() consulta: EncontrarTweetsParametrosDto) {
+	async encontrarTweets(
+		@Query() consulta: EncontrarTweetsParametrosDto,
+		@Res() res: Response,
+	) {
 		try {
 			const encontrado = await this.tweetsService.encontrarTweets(consulta);
 
 			if (encontrado.tweets.length === 0) {
-				return {
+				return res.status(HttpStatus.NOT_FOUND).json({
 					conteudo: encontrado,
 					mensagem: {
 						codigo: 404,
 						texto: 'Nenhum tweet foi encontrado',
 					},
 					status: false,
-				};
+				});
 			}
-			return {
+			return res.status(HttpStatus.OK).json({
 				conteudo: encontrado,
 				mensagem: {
 					codigo: 200,
 					texto: 'Tweets encontrados',
 				},
 				status: true,
-			};
+			});
 		} catch (error) {
-			return {
+			return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
 				conteudo: null,
 				mensagem: {
 					codigo: 500,
 					texto: 'Erro interno do servidor',
 				},
 				status: false,
-			};
+			});
 		}
 	}
 }
--- twitter-api/src/usuarios/usuarios.controller.ts
+++ twitter-api/src/usuarios/usuarios.controller.ts
@@ -5,21 +5,23 @@ import {
 	Controller,
 	Delete,
 	Get,
+	HttpStatus,
 	NotFoundException,
 	Patch,
 	Post,
 	Query,
+	Res,
 	UseGuards,
 	ValidationPipe,
 } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CriarUsuarioDto } from './dto/criar-usuario.dto';
-import { RetornoUsuarioDto } from './dto/retorno-usuario.dto';
 import { AlterarUsuarioDto } from './dto/alterar-usuario.dto';
 import { EncontrarUsuariosParametrosDto } from './dto/encontrar-usuarios-parametros.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetIdUsuario } from './decorator/get-id-usuario.decorator';
 import { AlterarSenhaDto } from './dto/alterar-senha.dto';
+import { Response } from 'express';
 
 @Controller('usuarios')
 export class UsuariosController {
@@ -28,36 +30,38 @@ export class UsuariosController {
 	@Post()
 	async criarUsuario(
 		@Body(ValidationPipe) criarUsuarioDto: CriarUsuarioDto,
-	): Promise<RetornoUsuarioDto> {
+		@Res() res: Response,
+	): Promise<Response> {
 		try {
 			const usuario = await this.usuariosService.criarUsuario(criarUsuarioDto);
-			return {
+			return res.status(HttpStatus.CREATED).json({
 				conteudo: usuario,
 				mensagem: {
 					codigo: 201,
 					texto: 'Usuário criado com sucesso.',
 				},
 				status: true,
-			};
+			});
 		} catch (error) {
-			if (error instanceof ConflictException)
-				return {
+			if (error instanceof ConflictException) {
+				return res.status(HttpStatus.CONFLICT).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 409,
 						texto: 'Endereço de email já está em uso',
 					},
 					status: false,
-				};
-			else
-				return {
+				});
+			} else {
+				return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 500,
 						texto: 'Erro interno do servidor',
 					},
 					status: false,
-				};
+				});
+			}
 		}
 	}
 
@@ -66,40 +70,40 @@ export class UsuariosController {
 	async alterarUsuario(
 		@Body(ValidationPipe) alterarUsuarioDto: AlterarUsuarioDto,
 		@GetIdUsuario() id: string,
+		@Res() res: Response,
 	) {
 		try {
 			const usuarioAlterado = await this.usuariosService.alterarUsuario(
 				alterarUsuarioDto,
 				id,
 			);
-
-			return {
+			return res.status(HttpStatus.OK).json({
 				conteudo: usuarioAlterado,
 				mensagem: {
 					codigo: 200,
 					texto: 'Usuário alterado com sucesso',
 				},
 				status: true,
-			};
+			});
 		} catch (error) {
 			if (error instanceof NotFoundException) {
-				return {
+				return res.status(HttpStatus.NOT_FOUND).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 404,
 						texto: 'Usuário não encontrado',
 					},
 					status: false,
-				};
+				});
 			} else {
-				return {
+				return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 500,
 						texto: 'Erro interno do servidor',
 					},
 					status: false,
-				};
+				});
 			}
 		}
 	}
@@ -109,6 +113,7 @@ export class UsuariosController {
 	async alterarSenha(
 		@Body(ValidationPipe) alterarSenhaDto: AlterarSenhaDto,
 		@GetIdUsuario() id: string,
+		@Res() res: Response,
 	) {
 		try {
 			const usuarioAlterado = await this.usuariosService.alterarSenha(
@@ -116,116 +121,119 @@ export class UsuariosController {
 				id,
 			);
 
-			return {
+			return res.status(HttpStatus.OK).json({
 				conteudo: usuarioAlterado,
 				mensagem: {
 					codigo: 200,
 					texto: 'Senha alterada com sucesso',
 				},
 				status: true,
-			};
+			});
 		} catch (error) {
 			if (error instanceof NotFoundException) {
-				return {
+				return res.status(HttpStatus.NOT_FOUND).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 404,
 						texto: 'Usuário não encontrado',
 					},
 					status: false,
-				};
+				});
 			} else if (error instanceof BadRequestException) {
-				return {
+				return res.status(HttpStatus.BAD_REQUEST).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 400,
 						texto: 'As senhas não conferem',
 					},
 					status: false,
-				};
+				});
 			} else {
-				return {
+				return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 500,
 						texto: 'Erro interno do servidor',
 					},
 					status: false,
-				};
+				});
 			}
 		}
 	}
 
 	@Delete()
 	@UseGuards(AuthGuard())
-	async deletarUsuario(@GetIdUsuario() id: string) {
+	async deletarUsuario(@GetIdUsuario() id: string, @Res() res: Response) {
 		try {
 			await this.usuariosService.deletarUsuario(id);
 
-			return {
+			return res.status(HttpStatus.OK).json({
 				conteudo: null,
 				mensagem: {
 					codigo: 200,
 					texto: 'Usuário removido com sucesso',
 				},
 				status: true,
-			};
+			});
 		} catch (error) {
-			if (error instanceof NotFoundException)
-				return {
+			if (error instanceof NotFoundException) {
+				return res.status(HttpStatus.NOT_FOUND).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 404,
 						texto: 'Usuario não encontrado',
 					},
 					status: false,
-				};
-			else {
-				return {
+				});
+			} else {
+				return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
 					conteudo: null,
 					mensagem: {
 						codigo: 500,
 						texto: 'Erro interno do servidor',
 					},
 					status: false,
-				};
+				});
 			}
 		}
 	}
 
 	@Get()
 	@UseGuards(AuthGuard())
-	async encontrarUsuarios(@Query() consulta: EncontrarUsuariosParametrosDto) {
+	async encontrarUsuarios(
+		@Query() consulta: EncontrarUsuariosParametrosDto,
+		@Res() res: Response,
+	) {
 		try {
 			const encontrado = await this.usuariosService.encontrarUsuarios(consulta);
 
 			if (encontrado.usuarios.length === 0) {
-				return {
+				return res.status(HttpStatus.NOT_FOUND).json({
 					conteudo: encontrado,
 					mensagem: {
 						codigo: 404,
 						texto: 'Nenhum usuário foi encontrado',
 					},
 					status: false,
-				};
+				});
 			}
-			return {
+			return res.status(HttpStatus.OK).json({
 				conteudo: encontrado,
 				mensagem: {
 					codigo: 200,
 					texto: 'Usuários encontrados',
 				},
 				status: true,
-			};
+			});
 		} catch (error) {
-			return {
+			return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
 				conteudo: null,
 				mensagem: {
 					codigo: 500,
 					texto: 'Erro interno do servidor',
 				},
 				status: false,
-			};
+			});
 		}
 	}
 }
