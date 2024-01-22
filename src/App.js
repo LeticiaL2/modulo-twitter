8459b92a47f86a94cc7763c050293c7cbf4fbe07
ThiagoSlvr <thiagosilvera09@gmail.com>
2024-01-22 16:43:24 -0300

    Add new routes and main App changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import TweetFeedPage from './pages/TweetFeed';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route  path="/feed" element={<TweetFeedPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/TweetFeed.js
@@ -0,0 +1,10 @@
+function TweetFeedPage() {
+    return (
+        <div>
+            <h1>Tweet Feeds Page</h1>
+        </div>
+    );
+}
+
+
+export default TweetFeedPage;
