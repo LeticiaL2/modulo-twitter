c2268d05aa3d4f7e1660c5b616d1b26e63b8df31
ThiagoSlvr <thiagosilvera09@gmail.com>
2024-01-23 19:24:41 -0300

    yup sign in schema

---
+++ src/schemas/SignInSchema.js
@@ -0,0 +1,13 @@
+import * as yup from 'yup';
+
+export const signInSchema = yup.object().shape({
+    email: yup
+        .string()
+        .required('Email é obrigatório')
+        .max(50, 'Email deve conter menos de 50 caracteres'),
+
+    senha: yup
+        .string()
+        .required('Senha é obrigatória')
+        .max(50, 'Senha deve conter menos de 50 caracteres'),
+});
