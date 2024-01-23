206a9b6d08dce1b5973d6b897bc339c8294a4d63
ThiagoSlvr <thiagosilvera09@gmail.com>
2024-01-23 19:25:11 -0300

    renamed userService function

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const registerUser = async (user) => {
+export const signUpUser = async (user) => {
     try {
         const response = await axios.post('http://localhost:3003/api/v1/usuarios', user);
         return response.data;
