c0df5487ce960e4cfd2d94288dc106345d8571da
ThiagoSlvr <thiagosilvera09@gmail.com>
2024-01-22 23:44:10 -0300

    modal css changes to fit smaller forms

--- src/components/organisms/AuthModal.js
+++ src/components/organisms/AuthModal.js
@@ -5,10 +5,11 @@ import './AuthModal.css';
 
 const AuthModal = ({ handleClose, show, children, isLogin }) => {
     const showHideClassName = show ? "modal display-block" : "modal display-none";
+    const formSizeClassName = isLogin ? "modal-small" : "modal-big";
 
     return (
         <div className={showHideClassName}>
-            <section className="modal-main">
+            <section className={`modal-main ${formSizeClassName}`}>
                 <div className="modal-close" onClick={handleClose}>x</div>
                 {isLogin ? <SignIn handleClose={handleClose} /> : <SignUp handleClose={handleClose} />}
             </section>
