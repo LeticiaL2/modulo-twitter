b4d4f60b6697450c2699e3b6e6420d86fe1b7a38
ThiagoSlvr <thiagosilvera09@gmail.com>
2024-01-30 14:40:24 -0300

    welcome to name changes

--- src/templates/HomeTemplate.js
+++ src/templates/HomeTemplate.js
@@ -1,34 +1,33 @@
 import AuthModal from '../components/organisms/AuthModal';
 import Button from '../components/atoms/Button';
 import Image from '../components/atoms/Image';
-import './HomeTemplate.css';
 
 const HomeTemplate = ({ show, isLogin, showModal, hideModal }) => (
-  <div className="welcome--container">
+  <div className="home--container">
     <AuthModal show={show} handleClose={hideModal} isLogin={isLogin} />
-    <div className="welcome--left">
-      <Image src="x_icon.png" alt="Welcome" />
+    <div className="home--left">
+      <Image src="x_icon.png" />
     </div>
-    <div className="welcome--right">
+    <div className="home--right">
       <h1>Acontecendo agora</h1>
       <h4>Inscreva-se hoje</h4>
       <Button
         type="button"
         onClick={() => showModal(false)}
-        className="welcome--create-account-btn"
+        className="home--create-account-btn"
       >
         Criar conta
       </Button>
-      <div className="welcome--separator-container">
-        <div className="welcome--separator-line"></div>
-        <span className="welcome--separator-text">ou</span>
-        <div className="welcome--separator-line"></div>
+      <div className="home--separator-container">
+        <div className="home--separator-line"></div>
+        <span className="home--separator-text">ou</span>
+        <div className="home--separator-line"></div>
       </div>
       <p>Já tem uma conta?</p>
       <Button
         type="button"
         onClick={() => showModal(true)}
-        className="welcome--sign-in-btn"
+        className="home--sign-in-btn"
       >
         Entrar
       </Button>
