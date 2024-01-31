46210adb7afa14691b1555e7d44b147555481bfb
ThiagoSlvr <thiagosilvera09@gmail.com>
2024-01-31 18:02:38 -0300

    new route for tweetDetails

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import TweetFeedPage from './pages/TweetFeedPage';
 import NotFound from './pages/NotFound';
+import TweetDetails from './pages/TweetDetailsPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/feed" element={<TweetFeedPage />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/feed/:id" element={<TweetDetails />} />
       </Routes>
     </Router>
   );
